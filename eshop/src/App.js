104c6b5815649e42321250475ca113f7bbe331a9
AhseneRemmouche <ahsene.remmouche@outlook.com>
2023-02-02 18:37:12 -0500

    added Home component

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,41 +1,12 @@
+import Header from './components/header/header.component';
+import Home from './components/home/home.component';
 import './App.css';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import SearchIcon from '@mui/icons-material/Search';
 
 const App = () => {
     return (
         <div className='App'>
-            <div className='header__logo'>
-                <StorefrontIcon
-                    className='header__logoImage'
-                    fontSize='large'
-                />
-                <h2 className='header__logoTitle'>eSHOP</h2>
-            </div>
-            <div className='header__search'>
-                <input
-                    className='header__searchInput'
-                    type='search'
-                />
-                <SearchIcon />
-            </div>
-            <div className='header__nav'>
-                <div className='nav__item'>
-                    <span className='nav__itemLineOne'>Hello Guest</span>
-                    <span className='nav__itemLineOne'>Sign In</span>
-                </div>
-
-                <div className='nav__item'>
-                    <span className='nav__itemLineOne'>Your</span>
-                    <span className='nav__itemLineOne'>Shop</span>
-                </div>
-
-                <div className='nav__item'>
-                    <ShoppingBasketIcon />
-                    <span className='nav__itemLineOne'>0</span>
-                </div>
-            </div>
+            <Header />
+            <Home />
         </div>
     );
 };
---
+++ eshop/src/components/home/home.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <div className='home'>
+                <div className='home__container'>
+                    <img
+                        src='https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg'
+                        alt=''
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
